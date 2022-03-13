0e201bc262a897f6656ec300b20b773dbfd8096b
pashkalini <pashkalini@users.noreply.github.com>
2022-03-13 21:34:07 +0300

    first commit

--- zdrav_bot_code.py
+++ zdrav_bot_code.py
@@ -11,8 +11,8 @@ from aiogram.types import ParseMode
 import config
 import logging
 from aiogram import Bot, Dispatcher, executor, types
-
 import mis_arianda
+import db_postgre as db
 
 API_TOKEN = config.TOKEN
 
@@ -21,7 +21,7 @@ logging.basicConfig(level=logging.INFO)
 
 # Initialize bot and dispatcher
 bot = Bot(token=API_TOKEN)
-storage = MemoryStorage()  # сюда БД
+storage = MemoryStorage()  # TODO сюда БД
 dp = Dispatcher(bot, storage=storage)
 
 
@@ -44,8 +44,9 @@ async def welcome(message: types.Message):
 #     keyboard.add("НАЗАД")
 #     return keyboard
 
-
-async def main_menu(message: types.Message):
+# Главное меню
+@dp.callback_query_handler(text="main_menu")
+async def main_menu(call: types.CallbackQuery):
     main_markup = types.InlineKeyboardMarkup(row_width=2)
     item1 = types.InlineKeyboardButton("ЗАПИСАТЬСЯ", callback_data='record')
     item2 = types.InlineKeyboardButton("МОИ ЗАПИСИ", callback_data='my_recordings')
@@ -53,7 +54,7 @@ async def main_menu(message: types.Message):
     item4 = types.InlineKeyboardButton("ЗАКЛЮЧЕНИЯ", callback_data='doctor_res')
     main_markup.add(item1, item2, item3, item4)
 
-    await message.answer("ГЛАВНОЕ МЕНЮ", reply_markup=main_markup)
+    await call.message.answer("ГЛАВНОЕ МЕНЮ", reply_markup=main_markup)
 
 
 async def lk_question(message: types.Message):
@@ -140,45 +141,101 @@ class RegForm(StatesGroup):
 
 @dp.callback_query_handler(text="lk_exists")
 async def authorisation_start(call: types.CallbackQuery):
-    await RegForm.login.set()  # Set state
+    await RegForm.login.set()  # задаем state (состояние) ввода логина
     await call.message.answer("Введите логин: \n(тот же, что Вы используете для "
                               "доступа к личному кабинету на сайте lk.med122.com)")
 
 
 @dp.message_handler(state=RegForm.login)
 async def process_name(message: types.Message, state: FSMContext):
+    #  Второй аргумент state типа FSMContext. Через него можно получить данные от FSM-бэкенда.
     async with state.proxy() as data:
-        data['login'] = message.text  # берем логин
-        await RegForm.next()
-        await message.answer("Введите пароль:", reply_markup=types.ReplyKeyboardRemove())
-        await state.update_data(passwd=message.text)
+        data['login'] = message.text  # сохраняем введенный пользователем логин
+        await RegForm.next()  # переходим к следующему состоянию - у нас это ввод пароля
+        await message.answer("Введите пароль:")
+        await state.update_data(passwd=message.text)  # обновляем data и state для сохранения пароля
 
 
 @dp.message_handler(state=RegForm.passwd)
 async def process_passwd(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
-        data['passwd'] = message.text  # берем пароль
+        data['passwd'] = message.text  # сохраняем введенный пароль
         await state.finish()
 
         # отправка POST-запроса с полученными логином и паролем для авторизации пользователя и получения токена
-        # ToDo возможно, тут стоит что-то вынести в отдельный метод именно для МИС Ариадна -- вынес
+        # возможно, тут стоит что-то вынести отдельно именно для МИС Ариадна -- вынесено
 
         login_response = mis_arianda.auth(data['login'], data['passwd'])
 
         if login_response.json().get("success"):
+            # если введены верные логин и пароль, то в ответ нам приходит токен для доступа к данным
             token = login_response.json().get("data").get("token")  # ToDo нужно сохранять токен в БД вместе с  chat_id
+            db.save_token(message.chat.id, token)
+
             print("Authorization: SUCCESS!")
-            get_info = mis_arianda.get_patient_info(token)
-            patient_lastname = get_info.get("lastname")
-            patient_firstname = get_info.get("firstname")
-            patient_secondname = get_info.get("secondname")
-            await message.answer(f"Добро Пожаловать, {patient_firstname} {patient_secondname}!")
-            await message.answer("Вам доступны следующие сервисы:")
-            await main_menu(message)
+            print(db.use_token(message.chat.id))
+            await auth_welcome(message)
+            # get_info = mis_arianda.get_patient_info(db.use_token(message.chat.id))
+            # patient_lastname = get_info.get("lastname")
+            # patient_firstname = get_info.get("firstname")
+            # patient_secondname = get_info.get("secondname")
+            # print(patient_lastname + patient_secondname + patient_firstname)
+            # await message.answer(f"Добро Пожаловать, {patient_firstname} {patient_secondname}!")
+            # await message.answer("Вам доступны следующие сервисы:")
 
         else:
             await message.answer("Логин/пароль неверный, попробуйте ещё раз")
             await lk_question(message)
+
+
+async def auth_welcome(message: types.Message):
+    welcome_menu = types.InlineKeyboardMarkup(row_width=1)
+    item1 = types.InlineKeyboardButton("ГЛАВНОЕ МЕНЮ", callback_data='main_menu')
+    item2 = types.InlineKeyboardButton("Информация о клинике", callback_data='clinic_info')
+    item3 = types.InlineKeyboardButton("Мои данные", callback_data='my_info')
+    welcome_menu.add(item1, item2, item3)
+
+    get_info = mis_arianda.get_patient_info(db.use_token(message.chat.id))
+    patient_lastname = get_info.get("lastname")
+    patient_firstname = get_info.get("firstname")
+    patient_secondname = get_info.get("secondname")
+    print(patient_lastname + patient_secondname + patient_firstname)
+    await message.answer(f"Добро Пожаловать, {patient_firstname} {patient_secondname}!", reply_markup=welcome_menu)
+
+
+
+
+''' 2.1 - сервис МОИ ЗАПИСИ '''
+
+
+@dp.callback_query_handler(text="my_recordings")
+async def registration_offer(call: types.CallbackQuery):
+    cancel_rec_btn = types.InlineKeyboardMarkup(row_width=1)
+    item1 = types.InlineKeyboardButton("Отменить запись", callback_data='cancel_recording')
+    item2 = types.InlineKeyboardButton("ГЛАВНОЕ МЕНЮ", callback_data='main_menu')
+    cancel_rec_btn.add(item1, item2)
+
+    all_recordings = mis_arianda.get_recordings(db.use_token(call.message.chat.id))
+    i = 0
+    if all_recordings:
+        for recording in all_recordings:
+            i += 1
+            await call.message.answer(f"Запись {i}:\n"
+                                      f"Дата и время приёма: {recording.get('dat_bgn')}\n"
+                                      f"Специалист: {recording.get('spec')}\n"
+                                      f"ФИО врача: {recording.get('lastname')} "
+                                      f"{recording.get('firstname')} {recording.get('secondname')}\n"
+                                      f"Место: {recording.get('depname')}\n"
+                                      f"Адрес: {recording.get('addr')}\n"
+                                      f"Кабинет: {recording.get('cab')}\n"
+                                      f"Телефон:  {recording.get('phone')}",
+                                      reply_markup=cancel_rec_btn)
+    else:
+        to_main_menu = types.InlineKeyboardMarkup(row_width=1)
+        item2 = types.InlineKeyboardButton("ГЛАВНОЕ МЕНЮ", callback_data='main_menu')
+        to_main_menu.add(item2)
+        await call.message.answer("У вас нет записей", reply_markup=to_main_menu)
+
 
 
 if __name__ == '__main__':
